10e47bc9a61b925601d5f2781ab1b8cc1983e321
sachin kumar biswal <sachinkbiswal@gmail.com>
2023-04-23 15:42:57 +0530

    HW 12

---
+++ src/Component/Main.jsx
@@ -0,0 +1,14 @@
+// import Card from "./Card";
+import Fetch from "./Fetch";
+import main from '../Component/main.css'
+
+const Main =() =>{
+    return(
+        <div>
+            <Fetch/>
+
+        </div>
+    )
+
+}
+export default Main;
